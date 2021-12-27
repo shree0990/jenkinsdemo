9ba64df35fa781be55c95f42838535ae70cae031
shree0990 <shrikantpawar0990@gmail.com>
2021-12-27 12:29:07 +0530

    Addedd jenkins demo

---
+++ jenkinsdemo/jenkins_demo/doctype/jenkins_demo/jenkins_demo.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Shree and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Jenkins Demo', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ jenkinsdemo/jenkins_demo/doctype/jenkins_demo/test_jenkins_demo.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Shree and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestJenkinsDemo(unittest.TestCase):
+	pass
